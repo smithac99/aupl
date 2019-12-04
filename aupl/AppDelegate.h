31e71f2179125dab4074c2b69d7308377eac97d4
alan <alan@onebillion.org>
2019-12-04 21:25:08 +0000

    Initial Commit

---
+++ aupl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  aupl
+//
+//  Created by alan on 04/12/19.
+//  Copyright © 2019 alancsmith. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ aupl/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  aupl
+//
+//  Created by alan on 04/12/19.
+//  Copyright © 2019 alancsmith. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
